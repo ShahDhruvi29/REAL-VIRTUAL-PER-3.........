26c70372c5825d715b8e44f834cd1cd8447a92aa
SHAH DHRUVI SUDHIR <75307389+ShahDhruvi29@users.noreply.github.com>
2021-02-01 15:34:55 +0530

    Add files via upload

---
+++ js/food.js
@@ -0,0 +1,45 @@
+class Foody{
+    constructor(){
+        this.foodStock = 20;
+        this.lastFed = 0;
+        this.x=0;
+        this.image = loadImage("Milk.png");
+    }
+    updateFoodStock(food){
+        this.foodStock = food;
+        database.ref('/').update({
+            Food: food
+        });
+    }
+    getFoodStock(){
+        return this.foodStock;
+    }
+    deductFood(){
+        this.foodStock--;
+        updateFoodStock(this.foodStock);
+    }
+        display(){
+        var x = 80, y = 100;
+        imageMode(CENTER);
+
+        if(this.foodStock != 0){
+            for(var i = 0; i < this.foodStock; i++){
+                if(i % 10 === 0){
+                   x = 80;
+                   y=y+50;
+                }
+                image(this.image, x, y, 70, 70);
+                x=x+30
+            }
+         }
+       }
+               bedroom(){
+               image(bedImg, 500,200, 1000, 600);
+               }     
+             garden(){
+                image(parkImg, 500,200, 1000, 600);
+             } 
+               washroom(){
+                image(bathImg, 500,200, 1000, 600);
+               }
+  }
